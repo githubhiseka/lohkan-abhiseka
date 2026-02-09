d57c38fec9d9e5e24f5193da1f1bbe1b2a0fe857
githubhiseka <susantoabhiseka@gmail.com>
2026-02-09 18:44:43 +0700

    fix: changed static root dir for demo setup

--- lohkan/settings.py
+++ lohkan/settings.py
@@ -135,12 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / 'static' # merujuk ke /static root project pada mode development
-    ]
-else:
-    STATIC_ROOT = BASE_DIR / 'static'
+# if DEBUG:
+#     STATICFILES_DIRS = [
+#         BASE_DIR / 'static' # merujuk ke /static root project pada mode development
+#     ]
+# else:
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
